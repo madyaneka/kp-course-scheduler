ed4b3e093e616164cb56c5acf797db272ad17a6b
Madyan <madyan.eka95@gmail.com>
2019-06-28 08:40:14 +0700

    using StackedInline

--- polls/admin.py
+++ polls/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date Information', {'fields': ['pub_date']})
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']})
     ]
+    inlines = [ChoiceInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
